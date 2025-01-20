d66c6749044240726b3fd0d5cc1d3e2f22113662
dhimasarista <dhimasharrisst@gmail.com>
2025-01-20 23:16:13 +0700

    create users resource controller

--- start/routes.ts
+++ start/routes.ts
@@ -7,6 +7,9 @@
 |
 */
 
+import UsersController from '#controllers/users_controller'
 import router from '@adonisjs/core/services/router'
 
 router.on('/').render('pages/home')
+
+router.resource("/users", UsersController)

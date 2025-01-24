2adb0fceaa04cc9f3d51b2db0f65d9412c250534
dhimasarista <dhimasharrisst@gmail.com>
2025-01-24 18:06:57 +0700

    feat: implement error handling with logger in ProductsController

--- app/controllers/products_controller.ts
+++ app/controllers/products_controller.ts
@@ -1,6 +1,8 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import { ProductService } from "#services/product_service";
 import { inject } from "@adonisjs/core";
+import logger from '@adonisjs/core/services/logger'
+import env from '#start/env';
 
 @inject()
 export default class ProductsController {
@@ -8,9 +10,30 @@ export default class ProductsController {
     protected productService: ProductService
   ){}
   async list({response}: HttpContext){
-    const products = await this.productService.list();
-    return response.json({
-      products
-    })
+    try {
+      const products = await this.productService.list();
+      return response.json({
+        products
+      });
+    } catch (error) {
+      if (error instanceof Error) {
+        logger.error({ err: Error }, error.message)
+        return response.status(500).json({
+          message: "internal server error"
+        });
+      }
+    }
+  }
+  async index({view}: HttpContext){
+    try {
+      return view.render("pages/list_product", {})
+    } catch (err) {
+      if (err instanceof Error) {
+        logger.error({ err: Error }, err.message);
+        if (env.get("NODE_ENV", "development")) {
+          return view.render("pages/errors/server_error", { error: err })
+        }
+      }
+    }
   }
 }

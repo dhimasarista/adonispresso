7a891546973bef01d5a006efe7c92bf4c13c5ffd
dhimasarista <dhimasharrisst@gmail.com>
2025-01-26 02:05:17 +0700

    feat: implement product creation and improve error handling in image upload methods

--- app/controllers/products_controller.ts
+++ app/controllers/products_controller.ts
@@ -3,12 +3,22 @@ import { ProductService } from "#services/product_service";
 import { inject } from "@adonisjs/core";
 import logger from '@adonisjs/core/services/logger'
 import env from '#start/env';
+import { ClientError, errorResponse, ServerError } from '../utilities/error_handling.js';
 
 @inject()
 export default class ProductsController {
   constructor(
     protected productService: ProductService
   ) { }
+  async store({response, request}: HttpContext){
+    try {
+      const data = request.only(["name", "image", "price"])
+      const createProduct = await this.productService.createProduct(data)
+      return response.status(200).json(createProduct)
+    } catch (error) {
+      errorResponse(error, response);
+    }
+  }
   async list({ response }: HttpContext) {
     try {
       const products = await this.productService.list();
@@ -68,46 +78,21 @@ export default class ProductsController {
  */
   async uploadImage({ request, response }: HttpContext) {
     try {
-      const image = request.file("filepond", {
-        size: "5mb",
-        extnames: ['jpeg', 'jpg', 'png'],
-      });
-      if (!image) throw new Error("missing image");
-      const uploadImage = await this.productService.uploadImage(image);
+      const uploadImage = await this.productService.uploadImage(request);
       return response.status(200).send(uploadImage);
     } catch (error) {
-      if (error instanceof Error) {
-        logger.error({ err: Error }, error.message)
-        if (error.message === "missing image") {
-          return response.status(400).json({
-            message: "missing image"
-          });
-        }
-        return response.status(500).json({
-          message: "internal server error"
-        });
-      }
+      errorResponse(error, response);
     }
   }
   public async deleteImage({ request, response }: HttpContext) {
     try {
       const body = request.only(["image"]);
       const deleteImage = await this.productService.deleteImage(body.image);
-      if (deleteImage) {
-        return response.status(200).json({
-          message: `Image ${body.image} was deleted`
-        })
-      }
-      return response.status(400).json({
-        message: `Image ${body.image} could not be found or deleted`
+      return response.status(200).json({
+        message: deleteImage,
       })
     } catch (error) {
-      if (error instanceof Error) {
-        logger.error({ err: Error }, error.message)
-        return response.status(500).json({
-          message: "internal server error"
-        });
-      }
+      errorResponse(error, response);
     }
   }
 }

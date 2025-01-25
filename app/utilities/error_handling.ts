4167f6eed4fea9853a52312f738da1222e620c21
dhimasarista <dhimasharrisst@gmail.com>
2025-01-26 02:05:24 +0700

    feat: add centralized error handling utilities for client and server errors

---
+++ app/utilities/error_handling.ts
@@ -0,0 +1,30 @@
+import type { HttpContext } from '@adonisjs/core/http'
+export class ClientError extends Error {
+  statusCode: number;
+  constructor(message: string, statusCode = 400) {
+    super(message);
+    this.name = 'ClientError';
+    this.statusCode = statusCode;
+  }
+}
+
+export class ServerError extends Error {
+  statusCode: number;
+  constructor(message: string, statusCode = 500) {
+    super(message);
+    this.name = 'ServerError';
+    this.statusCode = statusCode;
+  }
+}
+export function errorResponse(error: any, response: any) {
+  if (error instanceof ServerError) {
+    return response.status(error.statusCode).json({
+      message: error.message
+    });
+  }
+  if (error instanceof ClientError) {
+    return response.status(error.statusCode).json({
+      message: error.message
+    });
+  }
+}

3c5d9311168043b31493ba1c96125d943636a098
dhimasarista <dhimasharrisst@gmail.com>
2025-01-26 02:05:35 +0700

    feat: add product creation validation using vine

---
+++ app/validators/product.ts
@@ -0,0 +1,9 @@
+import vine from '@vinejs/vine'
+
+export const createProductValidator = vine.compile(
+  vine.object({
+    name: vine.string().trim().minLength(3),
+    price: vine.number().withoutDecimals().min(0),
+    image: vine.string().nullable(),
+  })
+)

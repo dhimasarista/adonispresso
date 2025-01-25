1b31987df464c44fe2fc34050f1b0e1419637c90
dhimasarista <dhimasharrisst@gmail.com>
2025-01-25 16:26:07 +0700

    feat: add deleteImage method to ProductService for image removal

--- app/services/product_service.ts
+++ app/services/product_service.ts
@@ -1,16 +1,41 @@
 import { cuid } from '@adonisjs/core/helpers'
 import Product from "#models/product";
 import { MultipartFile } from '@adonisjs/core/bodyparser';
-export class ProductService{
-  public async list(){
+import drive from '@adonisjs/drive/services/main'
+export class ProductService {
+  public async list() {
     return await Product.query().select("id", "name", "price", "image", "created_at").whereNull("deleted_at");
   }
-  public async uploadImage(image: MultipartFile){
+  public async uploadImage(image: MultipartFile) {
     const imageName = `${cuid()}.${image.extname}`;
     const key = `products/${imageName}`
 
     await image.moveToDisk(key);
     // return await drive.use().getUrl(key)
     return imageName;
+  }
+  public async deleteImage(image: string) {
+    const location = `/products/${image}`;
+    const d = drive.use("fs");
+    try {
+      // Cari produk berdasarkan nama gambar
+      const product = await Product.findBy("image", image);
+      // Periksa apakah file ada di storage
+      const exists = await d.exists(location);
+      if (product && exists) {
+        // Update produk (jika ada) untuk menghapus referensi gambar
+        if (product) {
+          await product.merge({ image: null }).save();
+        }
+        // Hapus file di storage (jika ada)
+        if (exists) {
+          await d.delete(location);
+        }
+        return true;
+      }
+      return false;
+    } catch (error) {
+      throw new Error(error);
+    }
   }
 }

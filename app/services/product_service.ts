e195cd7d9386af3b3191f7031ebc34644c10cf66
dhimasarista <dhimasharrisst@gmail.com>
2025-01-25 07:35:09 +0700

    feat: implement image upload functionality in ProductService

--- app/services/product_service.ts
+++ app/services/product_service.ts
@@ -1,7 +1,16 @@
+import { cuid } from '@adonisjs/core/helpers'
 import Product from "#models/product";
-
+import { MultipartFile } from '@adonisjs/core/bodyparser';
 export class ProductService{
   public async list(){
     return await Product.query().select("id", "name", "price", "image", "created_at").whereNull("deleted_at");
+  }
+  public async uploadImage(image: MultipartFile){
+    const imageName = `${cuid()}.${image.extname}`;
+    const key = `products/${imageName}`
+
+    await image.moveToDisk(key);
+    // return await drive.use().getUrl(key)
+    return imageName;
   }
 }

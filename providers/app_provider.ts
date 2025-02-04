a4662504cd7eb52b39614297ef8f556a3dbb0cfb
dhimasarista <dhimasharrisst@gmail.com>
2025-02-04 07:03:12 +0700

    feat: add AppProvider for MidtransService integration

---
+++ providers/app_provider.ts
@@ -0,0 +1,34 @@
+import type { ApplicationService } from '@adonisjs/core/types'
+import { MidtransService } from '../app/services/midtrans_service.js';
+export default class AppProvider {
+  constructor(protected app: ApplicationService) {}
+
+  /**
+   * Register bindings to the container
+   */
+  register() {
+    this.app.container.bind(MidtransService, () => {
+      return new MidtransService();
+    })
+  }
+
+  /**
+   * The container bindings have booted
+   */
+  async boot() {}
+
+  /**
+   * The application has been booted
+   */
+  async start() {}
+
+  /**
+   * The process has been started
+   */
+  async ready() {}
+
+  /**
+   * Preparing to shutdown the app
+   */
+  async shutdown() {}
+}

dd4026ac9bb3039323ba975a5bb521ab84c2e443
dhimasarista <dhimasharrisst@gmail.com>
2025-02-04 08:41:12 +0700

    feat: implement checkStatus method in MidtransService for transaction status retrieval

--- app/services/midtrans_service.ts
+++ app/services/midtrans_service.ts
@@ -6,11 +6,11 @@ import midtransClient from "midtrans-client";
 
 export class MidtransService {
   private snap: midtransClient.Snap;
-
+  private serverKey: string = env.get('MIDTRANS_SERVER_KEY') ?? "";
   constructor(){
     this.snap = new midtransClient.Snap({
       isProduction: env.get('MIDTRANS_ENV') === 'production',
-      serverKey: env.get('MIDTRANS_SERVER_KEY'),
+      serverKey: this.serverKey,
       clientKey: env.get('MIDTRANS_CLIENT_KEY'),
     });
   }
@@ -30,5 +30,60 @@ export class MidtransService {
       logger.error(error.message);
       throw new ServerError("internal server error", 500);
     }
+  }
+  async checkStatus(orderId: string) {
+    let status = null;
+    let response = null;
+    const url: string = `https://api.sandbox.midtrans.com/v2/${orderId}/status`;
+    const auth = Buffer.from(this.serverKey).toString("base64");
+
+    try {
+      const fetchResponse = await fetch(url, {
+        method: "GET",
+        headers: {
+          Authorization: `Basic ${auth}`
+        }
+      });
+
+      if (!fetchResponse.ok) {
+        throw new Error("Failed to fetch transaction status");
+      }
+
+      const result = await fetchResponse.json() as { transaction_status: string, fraud_status: string };
+      response = result;
+    } catch (error) {
+      console.error("Error occurred while checking status:", error);
+      throw new Error("Internal Server Error");
+    }
+    const transactionStatus = response.transaction_status;
+    if (transactionStatus != null) {
+      // set transaction status based on response from check transaction status
+      if (transactionStatus === "capture") {
+        if (response.fraud_status === "challenge") {
+          // TODO set transaction status on your database to 'challenge'
+          // e.g: 'Payment status challenged. Please take action on your Merchant Administration Portal
+          status = "challenged";
+        } else if (response.fraud_status === "accept") {
+          // TODO set transaction status on your database to 'success'
+          status = "success";
+        }
+      } else if (transactionStatus === "settlement") {
+        // TODO set transaction status on your database to 'success'
+        status = "success";
+      } else if (transactionStatus === "deny") {
+        // TODO you can ignore 'deny', because most of the time it allows payment retries
+        // and later can become success
+        status = "deny";
+      } else if (transactionStatus === "cancel" || transactionStatus === "expire") {
+        // TODO set transaction status on your database to 'failure'
+        status = "cancel";
+      } else if (transactionStatus === "pending") {
+        // TODO set transaction status on your database to 'pending' / waiting payment
+        status = "waiting";
+      }
+    } else {
+      status = "cancel";
+    }
+    return status;
   }
 }
